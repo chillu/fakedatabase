360d45d0460155883d9aca8510a74af9a1ca82c5
madmatt <signups@madman.net.nz>
2019-07-16 15:43:09 +1200

    Upgrade module to PHP7
    API Changes:
    - Update to use the Chillu\FakeDatabase namespace for classes
    - Rename FakeObject::create_from_array() to FakeObject::createFromArray() (PSR-2 requirement for static methods)
    - Update to require PHP >7.2 (could be relaxed, mainly requires this due to phpunit/phpunit:^8)
    - Remove unused FakeDatabaseException class
    
    Test changes:
    - Update to phpunit 8 (from 3.7)
    - Update code coverage from ~75% to 100%
    - Add PHP Code Sniffer and run across codebase
    - Update Travis config
    Misc changes:
    - Add .gitignore to keep dev requirements out of the module (including vendor)
    - Remove vendor folder and composer.lock from module
    - Add branch-alias to composer.json

--- tests/FakeDatabaseTest.php
+++ tests/FakeDatabaseTest.php
@@ -1,99 +1,146 @@
 <?php
-class FakeDatabaseTest extends PHPUnit_Framework_TestCase {
 
-	/** @var FakeDatabase */
-	protected $db;
+namespace Chillu\FakeDatabase\Tests;
 
-	protected function setUp() {
-		$path = sys_get_temp_dir() . '/FakeDatabaseTest_' . uniqid() . '.json';
-		touch($path);
-		$this->db = new FakeDatabase($path);
-	}
+use \LogicException;
+use Chillu\FakeDatabase\FakeDatabase;
+use Chillu\FakeDatabase\FakeObject;
+use Chillu\FakeDatabase\Tests\Fixtures\FakeDBTestObject;
+use Chillu\FakeDatabase\Tests\Fixtures\FakeDBTestOtherObject;
+use PHPUnit\Framework\TestCase;
 
-	protected function tearDown() {
-		$this->db->reset();
-	}
+class FakeDatabaseTest extends TestCase
+{
 
-	public function testGetSet() {
-		$objIn = new FakeDatabaseTest_Object(array('foo' => 'bar'));
-		$this->db->set('FakeDatabaseTest_Object', 'id1', $objIn);
-		$objOut = $this->db->get('FakeDatabaseTest_Object', 'id1');
-		$this->assertInstanceOf('FakeDatabaseTest_Object', $objOut);
-		$this->assertEquals($objOut->foo, 'bar');
-	}
+    /** @var FakeDatabase */
+    protected $db;
+    
+    /** @var string */
+    protected $path;
 
-	public function testUpdate() {
-		$objIn = new FakeDatabaseTest_Object(array(
-			'foo' => 'bar',
-			'nested' => array(
-				'bar' => 'baz'
-			)
-		));
-		$this->db->set('FakeDatabaseTest_Object', 'id1', $objIn);
+    protected function setUp(): void
+    {
+        $path = sys_get_temp_dir() . '/FakeDatabaseTest_' . uniqid() . '.json';
+        touch($path);
 
-		$objIn = new FakeDatabaseTest_Object(array(
-			'foo' => 'newbar',
-			'nested' => array(
-				'bar' => null,
-				'new' => 'newval'
-			)
-		));
-		$this->db->update('FakeDatabaseTest_Object', 'id1', $objIn);
-		
-		$objOut = $this->db->get('FakeDatabaseTest_Object', 'id1');
-		$this->assertInstanceOf('FakeDatabaseTest_Object', $objOut);
-		$this->assertEquals($objOut->foo, 'newbar', 'Updates existing toplevel values');
-		$this->assertEquals($objOut->nested->bar, null, 'Nulls nested values');
-		$this->assertEquals($objOut->nested->new, 'newval', 'Inserts new nested values');
-	}
+        $this->path = $path;
+        $this->db = new FakeDatabase($path);
+    }
 
-	public function testReset() {
-		$objIn = new FakeDatabaseTest_Object(array('foo' => 'bar'));
-		$this->db->set('FakeDatabaseTest_Object', 'id1', $objIn);
-		$this->db->reset();
-		$this->assertNull($this->db->get('FakeDatabaseTest_Object', 'id1'));
-	}
+    protected function tearDown(): void
+    {
+        $this->db->reset();
+    }
 
-	public function testToArray() {
-		$this->db->set('obj', 'obj1', new FakeDatabaseTest_Object(array('foo' => 'bar')));
-		$this->db->set('otherobj', 'obj2', new FakeDatabaseTest_OtherObject(array('foo' => 'bar')));
-		$this->assertEquals(
-			array(
-				'obj' => array(
-					'obj1' => array(
-						'_type' => 'FakeDatabaseTest_Object',
-						'foo' => 'bar',
-						'_key' => 'obj1'
-					)
-				),
-				'otherobj' => array(
-					'obj2' => array(
-						'_type' => 'FakeDatabaseTest_OtherObject',
-						'foo' => 'bar',
-						'_key' => 'obj2'
-					)
-				)
-			),
-			$this->db->toArray()
-		);
-	}
+    public function testGetSet()
+    {
+        $objIn = new FakeDBTestObject(array('foo' => 'bar'));
+        $this->db->set('FakeDBTestObject', 'id1', $objIn);
+        $objOut = $this->db->get('FakeDBTestObject', 'id1');
+        $this->assertInstanceOf(FakeDBTestObject::class, $objOut);
+        $this->assertEquals($objOut->foo, 'bar');
+    }
 
-	public function testFindNested() {
-		$this->db->set('mytype', 'obj1', new FakeDatabaseTest_Object(array(
-			'array' => array('foo' => 'bar', 'bar' => 'baz')
-		)));
-		$this->db->set('mytype', 'obj2', new FakeDatabaseTest_OtherObject(array(
-			'array' => array('foo' => 'not matching', 'bar' => 'not matching')
-		)));
-		$match = $this->db->find('mytype', 'array.foo', 'bar');
-		$this->assertInstanceOf('FakeObject', $match);
-		$this->assertEquals(
-			'bar',
-			$match->array['foo']
-		);
-	}
+    public function testUpdate()
+    {
+        $objIn = new FakeDBTestObject(array(
+            'foo' => 'bar',
+            'nested' => array(
+                'bar' => 'baz'
+            )
+        ));
+        $this->db->set('FakeDatabaseTest_Object', 'id1', $objIn);
 
+        $objIn = new FakeDBTestObject(array(
+            'foo' => 'newbar',
+            'nested' => array(
+                'bar' => null,
+                'new' => 'newval'
+            )
+        ));
+        $this->db->update('FakeDatabaseTest_Object', 'id1', $objIn);
+        
+        $objOut = $this->db->get('FakeDatabaseTest_Object', 'id1');
+        $this->assertInstanceOf(FakeDBTestObject::class, $objOut);
+        $this->assertEquals($objOut->foo, 'newbar', 'Updates existing toplevel values');
+        $this->assertEquals($objOut->nested->bar, null, 'Nulls nested values');
+        $this->assertEquals($objOut->nested->new, 'newval', 'Inserts new nested values');
+        
+        $this->db->update('TypeThatDoesNotExist', 'DummyKey', $fakeObj = new FakeObject());
+        $this->assertEquals($fakeObj, $this->db->get('TypeThatDoesNotExist', 'DummyKey'));
+    }
+
+    public function testReset()
+    {
+        $objIn = new FakeDBTestObject(array('foo' => 'bar'));
+        $this->db->set('FakeDatabaseTest_Object', 'id1', $objIn);
+        $this->db->reset();
+        $this->assertNull($this->db->get('FakeDatabaseTest_Object', 'id1'));
+    }
+
+    public function testToArray()
+    {
+        $expected = [
+            'obj' => [
+                'obj1' => [
+                    '_type' => FakeDBTestObject::class,
+                    'foo' => 'bar',
+                    '_key' => 'obj1'
+                ]
+            ],
+            'otherobj' => [
+                'obj2' => [
+                    '_type' => FakeDBTestOtherObject::class,
+                    'foo' => 'bar',
+                    '_key' => 'obj2'
+                ]
+            ]
+        ];
+        
+        $this->db->set('obj', 'obj1', new FakeDBTestObject(array('foo' => 'bar')));
+        $this->db->set('otherobj', 'obj2', new FakeDBTestOtherObject(array('foo' => 'bar')));
+        $this->assertEquals($expected, $this->db->toArray());
+        $this->assertEquals([0 => $this->db->get('obj', 'obj1')], $this->db->getAll('obj'));
+    }
+    
+    public function testGetPath()
+    {
+        $this->assertEquals($this->path, $this->db->getPath());
+        $this->assertFileExists($this->path);
+    }
+    
+    public function testFindWithoutType()
+    {
+        $this->assertSame(false, $this->db->find('InvalidType', 'key', 'value'));
+    }
+
+    public function testFindNested()
+    {
+        $this->db->set('mytype', 'obj1', new FakeDBTestObject(array(
+            'array' => array('foo' => 'bar', 'bar' => 'baz')
+        )));
+        $this->db->set('mytype', 'obj2', new FakeDBTestOtherObject(array(
+            'array' => array('foo' => 'not matching', 'bar' => 'not matching')
+        )));
+        $match = $this->db->find('mytype', 'array.foo', 'bar');
+        $this->assertInstanceOf(FakeObject::class, $match);
+        $this->assertEquals(
+            'bar',
+            $match->array['foo']
+        );
+        
+        $this->assertEquals(null, $this->db->find('mytype', 'unknownpart.foo', 'bar'));
+    }
+
+    public function testGetDataIsntReadable()
+    {
+        $this->expectException(LogicException::class);
+        chmod($this->db->getPath(), '200');
+        $data = $this->db->get('DummyType', 'DummyKey');
+    }
+    
+    public function testGetAllWithNoKey()
+    {
+        $this->assertSame(false, $this->db->getAll('InvalidType'));
+    }
 }
-
-class FakeDatabaseTest_Object extends FakeObject {}
-class FakeDatabaseTest_OtherObject extends FakeObject {}
---
+++ tests/fixtures/FakeDBTestOtherObject.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Chillu\FakeDatabase\Tests\Fixtures;
+
+use Chillu\FakeDatabase\FakeObject;
+
+class FakeDBTestOtherObject extends FakeObject
+{
+
+}
